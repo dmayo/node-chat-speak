f5176831219dc76c5fd7428c24dbaf2783c76af5
dmayo <dmayo2@mit.edu>
2014-08-24 16:24:58 -0400

    cleaning up

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ var http = require('http');
 //Mongo stuff
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/bughousechess');
+var db = monk('localhost:27017/chatspeak');
 var chatDB = db.get('chat');
 
-
-var players = 12;
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
+var players = 1; //start user id numbering at 1
 
 var app = express();
 var server = http.createServer(app);
@@ -61,20 +58,14 @@ function generateAuth () {
 io.sockets.on('connection', function (socket) {
 	var playerID;
     var roomName;
-    var numInRoom = []; //number of players that have joined a game, this needs to be global somehow
-    function getNumInGame(ID){
-		//result=collection.find( {gameID:ID} ).toArray();
-		//console.log(result);
-	}
+    var numInRoom = []; 
     socket.on('authenticate', function(id, code) {
         //checkID(id,code)
         var newCode;
         if(id) {
             playerID = id;
             //check(auth codes)
-            //
-            newCode = code;
-           
+            newCode = code;  
         } else {
             gen = generateAuth();
             playerID = gen[0];
@@ -82,29 +73,18 @@ io.sockets.on('connection', function (socket) {
             console.log("New User Connected:" + playerID);
         }
         socket.emit('updateAuth', playerID, newCode);
-       // callback("asd");
-        //console.log(playerID);
-        //callback("asd", " ");
     });
     
 	socket.on('joinRoom', function(room) {
         roomName = room;
-		numInGame=getNumInGame(room);
-		if(1){
-			socket.join(room);       
-            chatDB.find({room:room}, ['playerID', 'message'], function (err, docs){
-                socket.emit('resetChat', docs);
-                console.log(docs);
-            });
-            
-			numInRoom.room+=1;
-			console.log("Player: " + playerID + " joined Room: " + room);
-		}
-		else{
-			console.log("game is full!");
-			console.log(numInRoom.room);
-			console.log(room);
-		}
+    	socket.join(room);       
+        chatDB.find({room:room}, ['playerID', 'message'], function (err, docs){
+            socket.emit('resetChat', docs);
+            console.log(docs);
+        });
+        
+    	numInRoom.room+=1;
+    	console.log("Player: " + playerID + " joined Room: " + room);
     });
     socket.on('getChat', function (message) {
         var username = playerID;
@@ -118,26 +98,10 @@ io.sockets.on('connection', function (socket) {
     });
     socket.on('newRoom', function () {
         roomName = guid();
-		if(1){
-			socket.join(roomName);
-			numInRoom.room+=1;
-			console.log("Player: " + playerID + " joined Room: " + roomName);
-            io.sockets.emit('updateGameID', roomName);
-        }
-		else{
-			console.log("game is full!");
-			console.log(numInRoom.room);
-			console.log(roomName);
-		}
-    });
-    
-    socket.on('getMove', function(x1, y1, x2, y2) {
-        console.log(x1 + " " + y1 + " " + x2 + " " + y2);
-         io.to(roomName).emit('getMove', x1, y1, x2, y2);
-    });
-    
-    socket.on('newGame', function () {
-		numInRoom.gameID=1;
+		socket.join(roomName);
+		numInRoom.room+=1;
+		console.log("Player: " + playerID + " joined Room: " + roomName);
+        io.sockets.emit('updateGameID', roomName);
     });
 });
 server.listen(app.get('port'), function() {
